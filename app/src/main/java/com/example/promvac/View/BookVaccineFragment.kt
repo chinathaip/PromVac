c1a89c3ec7f8c6002c117b6eb8df3fb3dc4bdf6e
chinathaip <cartoonabe@gmail.com>
2022-06-28 20:49:26 +0700

    implemented the booking function. Need to fix the search function

--- app/src/main/java/com/example/promvac/View/BookVaccineFragment.kt
+++ app/src/main/java/com/example/promvac/View/BookVaccineFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.promvac.R
+import android.widget.RadioButton
+import com.example.promvac.Model.Vaccines
 import com.example.promvac.ViewModel.BookVaccineViewModel
+import com.example.promvac.databinding.FragmentBookVaccineBinding
+import java.util.*
 
 class BookVaccineFragment : Fragment() {
 
@@ -16,18 +19,41 @@ class BookVaccineFragment : Fragment() {
     }
 
     private lateinit var viewModel: BookVaccineViewModel
+    private lateinit var binding: FragmentBookVaccineBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_book_vaccine, container, false)
+    ): View {
+        binding=FragmentBookVaccineBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BookVaccineViewModel::class.java)
         // TODO: Use the ViewModel
+
+        binding.btnSubmit.setOnClickListener {
+            val patientName = binding.inputName.text.toString()
+            val chosenVaccine = binding.root.findViewById<RadioButton>(binding.vaccineGroup.checkedRadioButtonId).text.toString()
+            val calendar= binding.inputDate
+            val firstDoseDate= Date(calendar.year-1900,calendar.month,calendar.dayOfMonth)
+            val chosenHospital = binding.inputHospital.selectedItem.toString()
+            chosenVaccine.let{vaccineName->
+                viewModel.createVaccine(vaccineName,firstDoseDate,chosenHospital)
+            }.run{
+                //this = first dose vaccine created from .let{}
+                //we take booster dose every 3 months, so we do month + 3 for the next appointment
+                val secondDoseDate = Date(calendar.year-1900,calendar.month+3,calendar.dayOfMonth)
+                val secondDose = Vaccines(this.vacName,secondDoseDate,this.hospital)
+                viewModel.createNewAppointment(patientName,this,secondDose)
+            }
+
+//            viewModel.updateCurrentPatientID()
+
+        }
     }
 
 }
--- app/src/main/java/com/example/promvac/ViewModel/BookVaccineViewModel.kt
+++ app/src/main/java/com/example/promvac/ViewModel/BookVaccineViewModel.kt
@@ -7,26 +7,53 @@ import com.example.promvac.Model.Vaccines
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import java.util.*
 
 class BookVaccineViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 
     private val db= FirebaseFirestore.getInstance()
 
-    val currentPatientID = 4
+
 
     fun createNewAppointment(patientName:String,firstDose:Vaccines,secondDose:Vaccines)=
         CoroutineScope(IO).launch {
             val newPatient = Patients(patientName,firstDose,secondDose)
-            val patientAttribute = db.collection("patients").document(currentPatientID.toString())
-            patientAttribute.set(newPatient).let {
-                it.addOnSuccessListener {
-                    Log.i("LOL","Success")
-                }
-                it.addOnFailureListener{
-                    Log.i("LOL","Error $it")
+            var patientID = getCurrentPatientID{
+                val patientAttribute = db.collection("patients").document(it.toString())
+
+                patientAttribute.set(newPatient).let {
+                    it.addOnSuccessListener {
+                        Log.i("LOL","Success")
+                    }
+                    it.addOnFailureListener{
+                        Log.i("LOL","Error $it")
+                    }
                 }
             }
+
+
         }
+
+    fun createVaccine(vaccineName:String,firstDoseDate:Date,hospital:String):Vaccines{
+        val vacName:String?
+        when(vaccineName){
+            "Astrazeneca"->vacName="Astrazeneca"
+            "Pfizer"->vacName="Pfizer"
+            "Sinovac"->vacName="Sinovac"
+            "Johnson"->vacName="Johnson"
+            else->vacName="Unidentified"
+        }
+        return Vaccines(vacName,firstDoseDate,hospital)
+    }
+
+    fun getCurrentPatientID(callBack:(Int?)->Unit)= CoroutineScope(IO).launch {
+
+        db.collection("patients").get().addOnSuccessListener {
+            Log.i("LOL",it.size().toString())
+            callBack(it.size()+1)
+        }
+    }
 }
